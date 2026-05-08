89ac537203a7bbd68ee03398e112d21c963ba4c3
patara122 <patara.a@gmail.com>
2026-05-08 09:24:50 +0700

    add populate script

---
+++ src/populate-script.ts
@@ -0,0 +1,27 @@
+import { bootstrap,DefaultJobQueuePlugin } from '@vendure/core';
+import { populate } from '@vendure/core/cli';
+// Point this to your actual production config
+import { config } from './vendure-config'; 
+import path from 'path';
+
+const productsCsvFile = path.join(__dirname, 'products.csv');
+const populateConfig = {
+    ...config,
+    plugins: (config.plugins || []).filter(plugin => plugin !== DefaultJobQueuePlugin,)
+}
+
+populate(
+  () => bootstrap(populateConfig),
+   productsCsvFile
+)
+  .then(app => app.close())
+  .then(
+    () => {
+      console.log('✅ Successfully bulk imported product data');
+      process.exit(0);
+    },
+    err => {
+      console.error('❌ Error during import:', err);
+      process.exit(1);
+    }
+  );
